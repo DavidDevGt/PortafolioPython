94fc4e7fa456400b64a03e57fdd1cd1fbe2bb6a9
Davidvl <josuedavidvl18@gmail.com>
2023-08-18 20:37:29 -0600

    Actualicé la función principal y corregí los nombres de las operaciones

--- CalculadoraBasicaPy/calculadora.py
+++ CalculadoraBasicaPy/calculadora.py
@@ -11,4 +11,23 @@ def division(a, b):
     return a / b if b != 0 else "No se puede dividir entre cero."
 
 def main():
-    
+    ops = {
+        "1": suma,
+        "2": resta,
+        "3": multiplicacion,
+        "4": division
+    }1
+
+
+    print("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir")
+    seleccion = input(": ")
+
+    if seleccion not in ops:
+        print("Opción no válida, intenta de nuevo...")
+        return
+
+    num1, num2 = float(input("Número 1: ")), float(input("Número 2: "))
+    print("Resultado: ", ops[seleccion](num1, num2))
+
+if __name__ == "__main__":
+    main()
