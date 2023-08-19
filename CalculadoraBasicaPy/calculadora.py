baf30253e623f4c1c9c01e61b0c5f08bbc73d773
Davidvl <josuedavidvl18@gmail.com>
2023-08-18 20:26:17 -0600

    first commit

---
+++ CalculadoraBasicaPy/calculadora.py
@@ -0,0 +1,14 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b if b != 0 else "No se puede dividir entre cero."
+
+def main():
+    
