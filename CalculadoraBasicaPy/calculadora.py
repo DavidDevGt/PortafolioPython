dfd55d4401ae36cb77ee6e942567644204a7c1c9
Davidvl <josuedavidvl18@gmail.com>
2023-08-18 20:45:31 -0600

    Añadido menú interactivo en la terminal

--- CalculadoraBasicaPy/calculadora.py
+++ CalculadoraBasicaPy/calculadora.py
@@ -10,6 +10,16 @@ def multiplicacion(a, b):
 def division(a, b):
     return a / b if b != 0 else "No se puede dividir entre cero."
 
+def mostrar_menu():
+    """Despliega el menú de opciones al usuario."""
+    print("\nCalculadora:")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+    return input(": ")
+
 def main():
     ops = {
         "1": suma,
@@ -18,15 +28,18 @@ def main():
         "4": division
     }
 
-    print("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir")
-    seleccion = input(": ")
+    while True:  # Bucle para mostrar el menú hasta que el usuario decida salir
+        seleccion = mostrar_menu()
 
-    if seleccion not in ops:
-        print("Opción no válida, intenta de nuevo...")
-        return
+        if seleccion == '5':
+            break
+        elif seleccion not in ops:
+            print("Opción no válida, intenta de nuevo...")
+            continue
 
-    num1, num2 = float(input("Número 1: ")), float(input("Número 2: "))
-    print("Resultado: ", ops[seleccion](num1, num2))
+        num1 = float(input("Número 1: "))
+        num2 = float(input("Número 2: "))
+        print("Resultado: ", ops[seleccion](num1, num2))
 
 if __name__ == "__main__":
     main()
